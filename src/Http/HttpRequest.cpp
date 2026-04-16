e55ba6e7f3219c805ae0316490b9632a8bca3e6d
Maxim Kleverov <maximkleverov@icloud.com>
2026-04-16 18:23:33 +0300

    clean up parseStartLine, parseHttpMethode & setError

--- src/Http/HttpRequest.cpp
+++ src/Http/HttpRequest.cpp
@@ -1,4 +1,5 @@
 #include "HttpRequest.hpp"
+#include <algorithm>
 #include <cstddef>
 
 namespace webserv {
@@ -37,37 +38,69 @@ ParseOutcome HttpRequest::append(std::string_view bytes) {
 
 // METHOD SP TARGET SP VERSION
 // GET /smth HTTP/1.1
-bool HttpRequest::parseStartLine() {
-  size_t pos = _rawBuffer.find("\r\n");
-  if (pos == std::string::npos)
-    return false;
 
-  // extraction
+/*
+malformed start line	400
+unknown method syntax	400
+invalid headers	400
+invalid body format	400
+wrong HTTP version	505
+body too large	413
+headers too large (optional)	431
+*/
+bool HttpRequest::parseStartLine() {
+  std::string::size_type pos = _rawBuffer.find("\r\n");
+  if (pos == std::string::npos)
+    return false; // still need to wait for more data from T
+
+  // extract first line
   std::string line = _rawBuffer.substr(0, pos);
   _rawBuffer.erase(0, pos + 2);
 
+  // space checks
+  if (line.empty() || std::count(line.begin(), line.end(), ' ') != 2 ||
+      line.front() == ' ' || line.back() == ' ')
+    return (setError(400), false);
+
+  // space pos
   std::size_t sp1 = line.find(' ');
-  if (sp1 == std::string::npos)
-    return (setError(228), false); // need to check error codes
   std::size_t sp2 = line.find(' ', sp1 + 1);
-  if (sp2 == std::string::npos)
-    return (setError(228), false); // need to check error code
+
+  // extract parts
   _methodText = line.substr(0, sp1);
-  std::string target = line.substr(sp1 + 1, sp2 - sp1 - 1);
+  _target = line.substr(sp1 + 1, sp2 - sp1 - 1);
   _httpVersion = line.substr(sp2 + 1);
 
   // validation
   _method = parseHttpMethod(_methodText);
   if (_method == HttpMethod::Unknown)
-    return (setError(228), false); // need to check error code
-
-  if (_httpVersion != "HTTP/1.1") // only 1.1 ? 
-    return (setError(228), false); // need to check error code
-  _target = target;
+    return (setError(400), false);
+  if (_httpVersion != "HTTP/1.1")  // only 1.1 ?
+    return (setError(505), false);
+  if (_target.empty() || _target[0] != '/')
+    return (setError(400), false);
   parseTarget();
 
   _state = RequestParseState::Headers;
   return true;
 }
+
+HttpMethod parseHttpMethod(std::string_view method) noexcept{
+    if (method == "GET")
+        return HttpMethod::Get;
+    if (method == "POST")
+        return HttpMethod::Post;
+    if (method == "DELETE")
+        return HttpMethod::Delete;
+    return HttpMethod::Unknown;
+}
+
+void HttpRequest::setError(int statusCode) noexcept{
+    _errorStatus = statusCode;
+    _state = RequestParseState::Error;
+}
+
+
+
 
 } // namespace webserv
