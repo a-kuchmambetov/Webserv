792e1d203926e78f88b8a354ca0492fe8b1b1a80
Maxim Kleverov <maximkleverov@icloud.com>
2026-04-16 17:22:20 +0300

    dirty version of parseStartLine

--- src/Http/HttpRequest.cpp
+++ src/Http/HttpRequest.cpp
@@ -1,4 +1,5 @@
 #include "HttpRequest.hpp"
+#include <cstddef>
 
 namespace webserv {
 
@@ -32,6 +33,41 @@ ParseOutcome HttpRequest::append(std::string_view bytes) {
       return ParseOutcome::Error;
     }
   }
+}
+
+// METHOD SP TARGET SP VERSION
+// GET /smth HTTP/1.1
+bool HttpRequest::parseStartLine() {
+  size_t pos = _rawBuffer.find("\r\n");
+  if (pos == std::string::npos)
+    return false;
+
+  // extraction
+  std::string line = _rawBuffer.substr(0, pos);
+  _rawBuffer.erase(0, pos + 2);
+
+  std::size_t sp1 = line.find(' ');
+  if (sp1 == std::string::npos)
+    return (setError(228), false); // need to check error codes
+  std::size_t sp2 = line.find(' ', sp1 + 1);
+  if (sp2 == std::string::npos)
+    return (setError(228), false); // need to check error code
+  _methodText = line.substr(0, sp1);
+  std::string target = line.substr(sp1 + 1, sp2 - sp1 - 1);
+  _httpVersion = line.substr(sp2 + 1);
+
+  // validation
+  _method = parseHttpMethod(_methodText);
+  if (_method == HttpMethod::Unknown)
+    return (setError(228), false); // need to check error code
+
+  if (_httpVersion != "HTTP/1.1") // only 1.1 ? 
+    return (setError(228), false); // need to check error code
+  _target = target;
+  parseTarget();
+
+  _state = RequestParseState::Headers;
+  return true;
 }
 
 } // namespace webserv
